ae63748721573f426588cd83473d0d3d4befbda6
Christiana Routon <christiana.routon@gmail.com>
2018-03-04 18:26:22 -0600

    connected to API and search results appear on screen

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -2,7 +2,7 @@ app.controller('SearchController', ['StarwarsService', function(StarwarsService)
     console.log('SearchController created.');
     let self = this;
 
-    self.newSearch = StarwarsService.newSearch;
+    self.starwars = StarwarsService.starwars;
     self.searchFor = StarwarsService.searchFor;
     
   }]);
--- server/public/scripts/services/starwars.service.js
+++ server/public/scripts/services/starwars.service.js
@@ -2,7 +2,7 @@ app.service('StarwarsService', ['$http', function($http) {
     console.log('starwars service created');
     let self = this;
 
-    self.newSearch = {newSeach:{}};
+    self.starwars = {newSeach:{}, searchResults:[]};
 
     self.searchFor = function(inputSearch){
         console.log('in searchFor function', inputSearch.type, inputSearch.keyword);
@@ -11,6 +11,9 @@ app.service('StarwarsService', ['$http', function($http) {
             url: `https://swapi.co/api/${inputSearch.type}/?search=${inputSearch.keyword}`,
         }).then(function(response){
             console.log('Search results:', response.data.results);
+            self.starwars.searchResults = response.data.results;
+            console.log('searchResults array:', self.starwars.searchResults);
+            
         }).catch(function(error){
             console.error('Error finding search', error)
         })
